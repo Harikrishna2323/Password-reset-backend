648f8540282370a062ed784519c4b851db2029d5
harikrishna2323 <harikb23@gmail.com>
2021-11-03 16:40:05 +0530

    added cors

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 const express = require("express");
-const path = require('path')
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 const errorMiddleware = require("./middlewares/errors");
 
@@ -17,21 +20,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(fileUpload());
 
-if (process.env.NODE_ENV !== "PRODUCTION"){
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-    app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  });
 }
-
 
 //Import all routes
 
 const authRouter = require("./routes/auth");
 
 app.use("/api/v1", authRouter);
-
 
 //Middleware to handle errors
 app.use(errorMiddleware);
