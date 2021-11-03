59f7c23874bb1bad59556c6707ec2020c3c5d174
harikrishna2323 <harikb23@gmail.com>
2021-11-03 17:58:14 +0530

    changed url

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -55,10 +55,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
   //create reset password url
 
-  const resetUrl = `${process.env.FRONTEND_URL}/password/reset/${restToken}`;
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/password/reset/${restToken}`;
   const message = `Your password reset token is as follows:\n\n${resetUrl}\n\nIf you have not requested this email, please ignore it.`;
 
-  console.log("mail to be sent");
   try {
     await sendEmail({
       email: user.email,
