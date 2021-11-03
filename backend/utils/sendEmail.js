edd29bcd16378f62d9feb2378c65468cc8e68b37
harikrishna2323 <harikb23@gmail.com>
2021-11-03 18:35:48 +0530

    changed nodemailer service

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,8 +2,9 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
+    service: "gmail",
+    // host: process.env.SMTP_HOST,
+    // port: process.env.SMTP_PORT,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASSWORD,
